40c0fd4cb9d76e45505fa7ba49b5101604838daa
Abdul Ayub Ali <abdulayubali004@gmail.com>
2026-06-20 12:24:02 +0530

    setup: client and some minimal functionalities

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from "../components/home/Banner";
+import CallToAction from "../components/home/CallToAction";
+import Feature from "../components/home/Features";
+import Footer from "../components/home/Footer";
+import Hero from "../components/home/Hero";
+import Testimonials from "../components/home/Testimonials";
+
+const Home = () => {
+  return <>
+    <Banner/>
+    <Hero/>
+    <Feature/>
+    <Testimonials/>
+    <CallToAction/>
+    <Footer/>
+  </>;
+};
+
+export default Home;
